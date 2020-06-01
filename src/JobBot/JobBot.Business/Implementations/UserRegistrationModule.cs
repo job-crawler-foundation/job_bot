3a129769d3f6c441c5860083d21c24e2cbf159f0
dmytro shabanov <dmytro.shabanov98@gmail.com>
2020-06-01 16:37:17 +0300

    add registration

---
+++ src/JobBot/JobBot.Business/Implementations/UserRegistrationModule.cs
@@ -0,0 +1,16 @@
+﻿using JobBot.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace JobBot.Business.Implementations
+{
+    public class UserRegistrationModule
+    {
+        public async Task RegisterUser(JobBotDbContext ctx, Update hook)
+        {
+        }
+    }
+}
